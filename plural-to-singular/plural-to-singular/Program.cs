0ed30207bbb887ac0a936f4cd23b9874f5737ba8
sergenekici <sergenekici@outlook.com>
2017-05-18 19:40:04 +0300

    Project completed.

---
+++ plural-to-singular/plural-to-singular/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace plural_to_singular
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 : plural to singular");
+            Console.WriteLine("2 : singular to plural");
+            Console.Write("Please make your choice : ");
+
+            string selection = Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("Enter a word : "); 
+            string text = Console.ReadLine();
+
+            string result = null;
+
+            switch (selection)
+            {
+                case "1" :
+                    result = TranslationHelper.PluralToSingular(text);
+                    break;
+                case "2":
+                    result = TranslationHelper.SingularToPlural(text);
+                    break;
+
+                default:
+                    break;
+            }
+
+            Console.WriteLine("result : {0} ", result);
+
+             
+            Console.ReadKey();
+        }
+    }
+}
---
+++ plural-to-singular/plural-to-singular/TranslationHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Data.Entity.Design.PluralizationServices;
+
+namespace plural_to_singular
+{
+    public static class TranslationHelper
+    {
+        public static string PluralToSingular(string text)
+        {
+            var cult = new CultureInfo("en-US");
+            string singular = PluralizationService.CreateService(cult)
+                       .Singularize(text); 
+
+            return singular;
+        }
+
+
+        public static string SingularToPlural(string text)
+        {
+            var cult = new CultureInfo("en-US");
+            string plural = PluralizationService.CreateService(cult)
+                       .Pluralize(text);
+
+            return plural;
+        }
+         
+    }
+}
